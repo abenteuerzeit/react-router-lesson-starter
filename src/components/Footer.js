0e61dd1f1edff45598e63d7c0e6c633b74e5d4cb
abenteuerzeit <amroz.muzyka@gmail.com>
2022-11-25 12:09:01 +0100

    Implement UseHistory footer navigation

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,16 +1,21 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
 
 export default function Footer() {
 
   // Get the history object
+  const historyObject = useHistory();
+
 
   const goBack = () => {
     // imperatively redirect back
+    historyObject.goBack();
 
   }
 
   const goForward = () => {
     // imperatively redirect forward
+    historyObject.goForward();
     
   }
 
